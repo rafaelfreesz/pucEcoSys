a4482b403f17c5d9e337b5decc07ea5bf16dbd44
Rafael Freesz <rafaelfreesz@gmail.com>
2024-08-05 21:13:15 -0300

    Preparação para implementação da entidade Venda

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const fornecedor = require('./fornecedor.routes');
 const contato = require('./contato.routes');
 const entrada = require('./entrada.routes');
 const item_entrada = require('./item_entrada.routes');
+const venda = require('./venda.routes');
 
 const router = Router();
 
@@ -12,6 +13,7 @@ router.use('/fornecedores',fornecedor);
 router.use('/contatos',contato);
 router.use('/entradas',entrada);
 router.use('/item_entrada',item_entrada);
+router.use('/venda',venda);
 
 router.get('/', (request, response) =>{
     console.log("Hello World!")
