1dcf4bd38a222c1dd8d5237963efad06f51eed98
Rafael Freesz <rafaelfreesz@gmail.com>
2024-10-25 17:03:47 -0300

    Front: Configuração bootstrap, routing module, inclusão dos componentes Header e Not-Found

---
+++ ecosys-front/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+// import { ProdutosComponent } from './produtos/produtos.component';
+// import { FornecedoresComponent } from './fornecedores/fornecedores.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+const routes: Routes = [
+  // {path:'', component: ProdutosComponent},
+  // {path:'', component: NotFoundComponent},
+  // {path:'produtos', component: ProdutosComponent},
+  // {path:'fornecedores', component: FornecedoresComponent},
+  {path:'not-found', component: NotFoundComponent},
+  {path:'**', redirectTo: 'not-found'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- ecosys-front/src/app/app.module.ts
+++ ecosys-front/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
