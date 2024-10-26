68e067d142373b1c675c90bc00acf3afd08b1ea5
Rafael Freesz <rafaelfreesz@gmail.com>
2024-10-25 21:59:50 -0300

    Front: Criação do Model Produto

---
+++ ecosys-front/src/app/models/produto.model.ts
@@ -0,0 +1,7 @@
+export class Produto{
+    public id: number = 0;
+    public nome: string = "";
+    public descricao: string = "";
+    public precoVenda: number = 0;
+    public qtdEstoque: number = 0;   
+}
