c937256c3cdde8d8fa24a61c5379a1caab932dc9
Rafael Freesz <rafaelfreesz@gmail.com>
2024-10-26 16:12:45 -0300

    Inserindo pipe na requisição para retornar um Produto

--- ecosys-front/src/app/services/http.service.ts
+++ ecosys-front/src/app/services/http.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs/operators";
+import { Produto } from "../models/produto.model";
 
 @Injectable()
 export class HttpService{
@@ -9,6 +11,25 @@ export class HttpService{
     getTodosProdutos() {
 
         return this.http.get<any>('http://localhost:3000/produtos')
+        .pipe(
+            map( resposta => {
+                const produtos: Produto[] = []
+
+                for (const i in resposta){
+                    const produto:Produto = new Produto();
+
+                    produto.id = resposta[i].id;
+                    produto.nome = resposta[i].nome;
+                    produto.descricao = resposta[i].descricao;
+                    produto.qtd_estoque= resposta[i].qtd_estoque
+                    produto.preco_venda = resposta[i].preco_venda;
+
+                    produtos.push(produto);
+                }
+
+                return produtos
+            })
+        )
 
     }
 
