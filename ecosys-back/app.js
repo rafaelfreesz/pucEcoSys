dfdfa969cc6285a36d394c90bf615306ff5d88b3
Rafael Freesz <rafaelfreesz@gmail.com>
2024-10-25 21:55:48 -0300

    Back: Instalação do cors para troca de requisições

--- ecosys-back/app.js
+++ ecosys-back/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const routes = require('./routes');
+const cors = require('cors')
 
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use('/',routes)
 
 app.use((err,req,res,next) => {
