4eb71a93cb7472dd7493f18d691a4b60fe15d184
Rafael Freesz <rafaelfreesz@gmail.com>
2024-10-29 22:33:24 -0300

    criando get para id de Fornecedor

--- ecosys-front/src/app/models/fornecedor.model.ts
+++ ecosys-front/src/app/models/fornecedor.model.ts
@@ -2,11 +2,18 @@ import { Contato } from "./contato.model";
 import { Endereco } from "./endereco.model";
 
 export class Fornecedor{
-    public id?: number = -1;
+    private _id?: number = -1; 
     public cnpj: string = "";
     public razao_social: string = "";
     public nome_empresarial: string = "";
-    public qtd_estoque: number = 0;
-    public endereco: Endereco | null = null;
+    public endereco: Endereco = new Endereco();
     public contatos: Contato[] = [];   
+
+    get id(): number{
+        return this._id? this._id : -1;
+    }
+
+    set id(value: number){
+        this._id = value
+    }
 }
