61b3397a34b1356a0a60b79fdecdd34612aceee1
Rafael Freesz <rafaelfreesz@gmail.com>
2024-11-23 23:33:33 -0300

    front: Preparando router module para seção de Entradas

--- ecosys-front/src/app/app-routing.module.ts
+++ ecosys-front/src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ProdutosComponent } from './produtos/produtos.component';
 import { HomeComponent } from './home/home.component';
 import { FornecedoresComponent } from './fornecedores/fornecedores.component';
+import { EntradasComponent } from './secoes/entradas/entradas.component';
 
 const routes: Routes = [
-  {path:'', component: FornecedoresComponent},
+  {path:'', component: HomeComponent},
   {path:'produtos', component: ProdutosComponent},
   {path:'fornecedores', component: FornecedoresComponent},
-  // {path:'', component: NotFoundComponent},
+  {path:'entradas', component: EntradasComponent},
   {path:'not-found', component: NotFoundComponent},
   {path:'**', redirectTo: 'not-found'}
 ];
