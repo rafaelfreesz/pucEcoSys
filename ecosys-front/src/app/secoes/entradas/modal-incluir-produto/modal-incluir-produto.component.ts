71321c0d5cf983d2cf8a2789d3c21b2b92910b21
Rafael Freesz <rafaelfreesz@gmail.com>
2025-01-21 12:36:05 -0300

    front: Permitindo selecionar produto e preencher campos

--- ecosys-front/src/app/secoes/entradas/modal-incluir-produto/modal-incluir-produto.component.ts
+++ ecosys-front/src/app/secoes/entradas/modal-incluir-produto/modal-incluir-produto.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { Produto } from 'src/app/models/produto.model';
 import { ProdutoService } from 'src/app/services/produto.service';
 
 @Component({
@@ -12,9 +13,12 @@ export class ModalIncluirProdutoComponent implements OnInit, OnDestroy {
 
   @Output() fecharModal: EventEmitter<any> = new EventEmitter<any>();
   todosProdutos: any[] = []
+  produtoSelecionado: any;
   produtosAlterados: Subscription;
   valorFiltro: string = "";
   criterioFiltro: string = "nome";
+  precoCompra: number = 0.01;
+  quantidade: number = 1;
   constructor(private produtoService: ProdutoService) {
     this.produtosAlterados = this.produtoService.produtosAlterados.subscribe(
         todosProdutos => {
@@ -36,7 +40,13 @@ export class ModalIncluirProdutoComponent implements OnInit, OnDestroy {
   }
 
   selecionar(produto: any){
-    this.fecharModal.emit(produto);
+    if(this.produtoSelecionado && produto.id === this.produtoSelecionado.id){
+      this.produtoSelecionado = null
+    }else{
+
+      this.produtoSelecionado = produto;
+    }
+    //this.fecharModal.emit(produto);
   }
 
 }
