f244cbe43ce1a6f423262db5b143ba61e7574d81
Rafael Freesz <rafaelfreesz@gmail.com>
2025-02-23 21:26:04 -0300

    front: Instalação do ChartJS e plotando um gráfico genérico

--- ecosys-front/src/app/home/home.component.ts
+++ ecosys-front/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Chart, registerables} from 'chart.js'
 
+Chart.register(...registerables)
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public config: any = {
+    type: 'line',
+    data: {
+      labels: ["Jan","Feb","Mar","April"],
+      datasets: [
+        {
+          label: "Sales",
+          data: ['467','576','572','588'],
+          backgroundColor: 'blue'
+        },
+        {
+          label: "Pat",
+          data: ['100','200','300','400'],
+          backgroundColor: 'red'
+        }
+      ]
+    }
+  }
+  chart: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.chart = new Chart('chart',this.config)
   }
 
 }
