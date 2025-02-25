2e188aa4d58c6eca6b1c534e0c60a5bdcc2bda8d
Rafael Freesz <rafaelfreesz@gmail.com>
2025-02-25 12:40:34 -0300

    front: Implementação card Redumo diário e refatoração do card de notificações

--- ecosys-front/src/app/home/resumo-do-dia/resumo-do-dia.component.ts
+++ ecosys-front/src/app/home/resumo-do-dia/resumo-do-dia.component.ts
@@ -7,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumoDoDiaComponent implements OnInit {
 
-  constructor() { }
+  dados: any = {
+    total_valor_vendas: 10,
+    total_vendas: 50,
+    valor_medio_vendas: 50/10,
+    produtos_vendidos: []
+  }
+
+  constructor() {
+    for(let i = 0; i<15; i++){
+      this.dados.produtos_vendidos.push(
+        {
+          nome: "ABC",
+          quantidade: 10,
+          valor_total: 10
+        }
+      )
+    }
+  }
 
   ngOnInit(): void {
   }
