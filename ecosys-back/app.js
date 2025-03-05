6b8b1aec3a1c4fb914a98a608a66c1b4e79b8e82
Rafael Freesz <rafaelfreesz@gmail.com>
2025-03-05 13:16:35 -0300

    back: Alterando permissões do back para receber imagens e diversificação das fronteiras

--- ecosys-back/app.js
+++ ecosys-back/app.js
@@ -6,11 +6,13 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit:'10mb'}));
 app.use(cors())
 app.use('/',routes)
 
 app.use((err,req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.json(err);
 })
 
