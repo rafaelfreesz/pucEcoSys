daab063af3ca5710442b53832d8ef412d111edc5
Rafael Freesz <rafaelfreesz@gmail.com>
2025-03-13 11:27:15 -0300

    Criando card de login e linkando com o reactive form

--- ecosys-front/src/app/secoes/login/login.component.ts
+++ ecosys-front/src/app/secoes/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -9,11 +10,22 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  conteudoFormulario: FormGroup | any;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.conteudoFormulario = new FormGroup({
+      'login': new FormControl(null, [Validators.required,Validators.minLength(4),Validators.maxLength(14)]),
+      'senha': new FormControl(null, [Validators.required,Validators.minLength(4),Validators.maxLength(14)])
+    })
+  }
 
   login(){
-    this.authService.login();
-    this.router.navigate(["/"])
+    console.log(this.conteudoFormulario.value)
+    // this.authService.login();
+    // this.router.navigate(["/"])
+  }
+  esqueciSenha(){
+    console.log("Esqueci a senha")
   }
   ngOnInit(): void {
   }
